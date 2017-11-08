bdbc204e96728878cfbf132b76c92d0e237f7e1d
Santa <santazaaa@gmail.com>
2017-11-08 23:29:00 +0700

    Login with custom id

---
+++ Assets/Scripts/TestPlayfab.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+
+public class TestPlayfab : MonoBehaviour
+{
+
+	void Start()
+	{
+		PlayFabClientAPI.LoginWithCustomID(
+			new LoginWithCustomIDRequest()
+			{
+				TitleId = "CA70",
+				CustomId = "8fa79815413d472d",
+				CreateAccount = true
+			},
+			(result) =>
+			{
+				Debug.Log("Login result: " + result);
+			},
+			(error) =>
+			{
+				Debug.LogError("Login error: " + error);
+			}
+		);
+	}
+	
+	void Update()
+	{
+		
+	}
+}
